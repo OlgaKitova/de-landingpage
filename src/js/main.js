9c53cbdea57d6dd2bee02bdfaeb6cf30a3f6758a
Olga Kitova <olga.vizgalova@bk.ru>
2021-03-12 13:03:08 +0500

    feat: add modal form

--- src/js/main.js
+++ src/js/main.js
@@ -9,4 +9,28 @@ window.addEventListener('DOMContentLoaded', () => {
     burger.classList.toggle('active');
     menu.classList.toggle('active');
   })
+
+  //Modals
+  const modal = document.querySelector('.popup__form'),
+        modalClose = document.querySelector('.popup__content .popup__close'),
+        btnModal = document.querySelector('.footer__btn'),
+        header = document.querySelector('.header');
+
+  btnModal.addEventListener('click', () => {
+      modal.classList.add('show');
+      header.style.display = 'none';
+    });
+
+  modalClose.addEventListener('click', () => {
+    modal.classList.remove('show');
+    header.style.display = 'block';
+
+  });
+  modal.addEventListener('click', (e) => {
+    if (e.target == modal) {
+      modal.classList.remove('show');
+      header.style.display = 'block';
+    }
+  });
+
 })
